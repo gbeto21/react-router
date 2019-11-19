d223fba02a606d4642f2c96ca9f33387557321e2
Alberto González Balderas <albertoglezb@hotmail.com>
2019-11-19 14:49:05 -0600

    Se accedió al historial desde un componente que no está dentro de la página, se utilizó la opción de withRouter. Se terminó la clase: 'Obteniendo el historial desde cualquier componente'.

--- src/pages/components/header.js
+++ src/pages/components/header.js
@@ -2,8 +2,14 @@ import React, { Component } from 'react';
 import './header.css';
 import { Link, NavLink } from 'react-router-dom';
 import logo from '../../../images/logo.png';
+import { withRouter } from 'react-router'
 
 class Header extends Component {
+
+  handleClick = () => {
+    this.props.history.goBack()
+  }
+
   render() {
     return (
       <header className="Header">
@@ -36,6 +42,9 @@ class Header extends Component {
               <NavLink to="/perfil" activeClassName="is-selected">
                 Perfil
               </NavLink>
+              <a onClick={this.handleClick}>
+                LINK
+              </a>
             </li>
           </ul>
         </nav>
@@ -44,4 +53,4 @@ class Header extends Component {
   }
 }
 
-export default Header
+export default withRouter(Header)
